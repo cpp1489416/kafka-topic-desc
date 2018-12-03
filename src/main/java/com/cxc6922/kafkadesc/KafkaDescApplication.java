bfb71f5988af91bc294e8fe3180a015a4cea18a7
chenxiaochen <chenxiaochen@shein.com>
2018-12-03 16:57:32 +0800

    init

---
+++ src/main/java/com/cxc6922/kafkadesc/KafkaDescApplication.java
@@ -0,0 +1,12 @@
+package com.cxc6922.kafkadesc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDescApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaDescApplication.class, args);
+    }
+}
